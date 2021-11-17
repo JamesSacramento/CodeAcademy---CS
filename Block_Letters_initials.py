d9cf528af9864a24d872456f9ff898dcbb8fd66c
James Noel Sacramento <36490528+jncsacramento@users.noreply.github.com>
2021-11-18 05:37:17 +0800

    Create Block_Letters_initials.py
    ASCII art a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters.

---
+++ Block_Letters_initials.py
@@ -0,0 +1,14 @@
+name = """
+JJJJJ N   N  CCC    SSS 
+  J   NN  N C   C  S   S
+  J   N N N C      S
+  J   N  NN C       SSS 
+J J   N   N C           S
+J J   N   N C   C  S    S
+ JJ   N   N  CCC    SSSS
+"""
+
+ #James Noel C. Sacramento
+ #Transtioning Careers
+
+print(name)
